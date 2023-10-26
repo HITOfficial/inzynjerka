64e6101e840d1d90675c7e8b65217c21e46025ab
hit <tomaszbochnak00@gmail.com>
2023-10-26 02:26:46 +0200

    ensure secret plug connection fix

--- lib/chat_api_web/ensure_secret_plug.ex
+++ lib/chat_api_web/ensure_secret_plug.ex
@@ -1,37 +1,33 @@
 defmodule ChatApiWeb.EnsureSecretPlug do
-  @moduledoc """
-  This plug ensures that a user has a particular role.
-
-  ## Example
-
-      plug ChatApiWeb.EnsureRolePlug, [:user, :admin]
-
-      plug ChatApiWeb.EnsureRolePlug, :admin
-
-      plug ChatApiWeb.EnsureRolePlug, ~w(user admin)a
-  """
   import Plug.Conn, only: [halt: 1, put_status: 2]
 
-  alias Hex.Application
   alias Phoenix.Controller
   alias Plug.Conn
   alias Pow.{Plug, Config}
 
-  def init(default), do: default
+  use Application
 
-  @doc false
-  @spec call(Conn.t(), any()) :: Conn.t()
+  def init(default), do: default
   def call(conn, _default) do
     sent_secret = Conn.get_req_header(conn, "authorization")
     secret = Application.get_env(:chat_api, :service_secret)
 
-    secret == sent_secret |> maybe_halt(conn)
+    case secret do
+      nil ->
+        conn
+        |> put_status(500)
+        |> Controller.json(%{error: %{status: 500, message: "Service secret is not configured"}})
+        |> halt()
+      _ ->
+        if sent_secret == secret do
+          conn
+        else
+          maybe_halt(conn)
+        end
+    end
   end
 
-  defp maybe_halt(true, conn), do: conn
-
-  defp maybe_halt(_any, conn) do
-    # TODO: figure out a better way to handle this
+  defp maybe_halt(conn) do
     conn
     |> put_status(401)
     |> Controller.json(%{error: %{status: 401, message: "Unauthorized access"}})
